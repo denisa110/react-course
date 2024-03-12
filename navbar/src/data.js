c38548b152ac5d252e40f78d9c8333a4cc0fdba6
Denisa Lupancu <denisalupancu@gmail.com>
2024-03-12 16:36:47 +0200

    First commit

---
+++ navbar/src/data.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { FaFacebook, FaLinkedin, FaGithub  } from 'react-icons/fa'
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'random',
+  },
+  {
+    id: 2,
+    url: '/about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    url: '/projects',
+    text: 'projects',
+  },
+  {
+    id: 4,
+    url: '/contact',
+    text: 'contact',
+  },
+  {
+    id: 5,
+    url: '/profile',
+    text: 'profile',
+  },
+]
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.facebook.com/profile.php?id=100008201166556',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://github.com/denisa110',
+    icon: <FaGithub  />,
+  },
+  {
+    id: 3,
+    url: 'https://www.linkedin.com/in/denisa-petronela-lupancu-6a7966264/',
+    icon: <FaLinkedin />,
+  },
+ 
+]
